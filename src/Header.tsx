9bd5717a5b824b719a0b75e86f28798744970125
Stanislav Ushakov <siushakov@inno.tech>
2025-04-06 22:39:23 +0300

    F11 - fullscreen

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const { gameOver, youWin, isPaused } = gameContext as gameContextProps;
   return (
     <div className="header">
-      <div className="actions-text">F5 - restart</div>
+      <div className="actions-text">{gameOver ? "F5 - restart" : "F11 - full screen"}</div>
       <div className="game-text-board">
         <AnimatedCircles isReversed />
         <div className="game-text">{gameOver ? "game over" : youWin ? "you win" : "just snake it"}</div>
